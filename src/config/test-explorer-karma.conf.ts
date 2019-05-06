d9596f6375c5096000c6ecf4c90c4779d9a616a5
Patricio Ferraggi Ares <pattferraggi@gmail.com>
2019-05-06 14:02:50 +0200

    remove unused code

--- src/config/test-explorer-karma.conf.ts
+++ src/config/test-explorer-karma.conf.ts
@@ -1,22 +1,10 @@
 import { KarmaConfigurator } from "./karma-configurator";
 import { Config } from "karma";
-// import * as path from "path";
 
-// let originalConfigPath = path.join(process.cwd(), "src", "karma.conf.js");
 const karmaConfigurator = new KarmaConfigurator();
-global.console.log(process.env.userKarmaConfigPath);
 const originalConfigPath = process.env.userKarmaConfigPath as string;
 
-// const setupCorrectKarmaConfFilePath = () => {
-//   try {
-//      require.resolve(originalConfigPath);
-//   } catch (error) {
-//      originalConfigPath = path.join(process.cwd(), "karma.conf.js");
-//   }
-// };
-
 module.exports = (config: Config) => {
-  // setupCorrectKarmaConfFilePath();
   karmaConfigurator.loadOriginalUserConfiguration(config, originalConfigPath);
   karmaConfigurator.setMandatoryOptions(config);
   karmaConfigurator.cleanUpReporters(config);
