39886d16a74b40f0bad626629d179baf2ae9bbd0
Patricio Ferraggi Ares <pattferraggi@gmail.com>
2019-06-17 14:24:33 +0200

    pathfinder tests working, ready to try/refactor

--- __tests__/core/shared/path-finder.tests.ts
+++ __tests__/core/shared/path-finder.tests.ts
@@ -1,6 +1,8 @@
 import { PathFinder } from "../../../src/core/shared/path-finder";
-import * as mock from "mock-require";
+import { FileHelper } from "../../../src/core/integration/file-helper";
 import * as lodash from "lodash";
+
+jest.mock("../../../src/core/integration/file-helper");
 
 const testFileData: any = {
   "path/t1.spec.js": "describe('s1', () => { it('d1', () => {}); });",
@@ -25,40 +27,39 @@ const parsedTestFiles: any = {
   "path/t6.spec.js": { describe: ["s6", "s6.2", "s6.3", "s6.4", "s6.4.1"], it: ["d6.1", "d6.2", "d6.3", "d6.4.1"] },
 };
 
-describe("Path finder tests", () => {
-  const pathFinder = new PathFinder();
+let fileHelper: jest.Mocked<FileHelper>;
+let pathFinder: PathFinder;
 
-  beforeEach(() => {
-    mock("fs", {
-      readFileSync: (path: any, encoding: any) => {
-        return testFileData[path];
-      },
-    });
+describe("Path finder tests", () => {
+  beforeAll(() => {
+    fileHelper = new (FileHelper as any)() as any;
+    fileHelper.readFile.mockImplementation((path: string, encoding: any) => testFileData[path]);
+    pathFinder = new PathFinder(fileHelper);
   });
 
   describe("Parsing test files", () => {
     describe("Test files with single test cases", () => {
       it("Parsed test paths matched expected test paths", () => {
-        const result = pathFinder.parseTestFiles("**/*.spec.ts", "utf-8");
+        const result = pathFinder.getTestFilesPaths("**/*.spec.ts", "utf-8");
         expect(lodash.isEqual(result, parsedTestFiles)).toBe(true);
       });
 
       it("1st test file match suite 1 and description 1", () => {
-        const paths: any = pathFinder.parseTestFiles("**/*.spec.ts", "utf-8");
+        const paths: any = pathFinder.getTestFilesPaths("**/*.spec.ts", "utf-8");
         const path: any = paths["path/t1.spec.js"];
         expect(path.describe[0]).toBe("s1");
         expect(path.it[0]).toBe("d1");
       });
 
       it("2sd test file match suite 2 and description 2", () => {
-        const paths: any = pathFinder.parseTestFiles("**/*.spec.ts", "utf-8");
+        const paths: any = pathFinder.getTestFilesPaths("**/*.spec.ts", "utf-8");
         const path: any = paths["path/t2.spec.js"];
         expect(path.describe[0]).toBe("s2");
         expect(path.it[0]).toBe("d2");
       });
 
       it("3rd test file match suite 3 and description 3.1 and 3.2", () => {
-        const paths: any = pathFinder.parseTestFiles("**/*.spec.ts", "utf-8");
+        const paths: any = pathFinder.getTestFilesPaths("**/*.spec.ts", "utf-8");
         const path: any = paths["path/t3.spec.js"];
         expect(path.describe[0]).toBe("s3");
         expect(path.it[0]).toBe("d3.1");
@@ -67,14 +68,14 @@ describe("Path finder tests", () => {
 
       describe("Test cases with quoted text", () => {
         it("4rd test file match suite and description with single quotes", () => {
-          const paths: any = pathFinder.parseTestFiles("**/*.spec.ts", "utf-8");
+          const paths: any = pathFinder.getTestFilesPaths("**/*.spec.ts", "utf-8");
           const path: any = paths["path/t4.spec.js"];
           expect(path.describe[0]).toBe("\\'s4\\'");
           expect(path.it[0]).toBe("\\'d4\\'");
         });
 
         it("5th test file match suite and description with double quotes", () => {
-          const paths: any = pathFinder.parseTestFiles("**/*.spec.ts", "utf-8");
+          const paths: any = pathFinder.getTestFilesPaths("**/*.spec.ts", "utf-8");
           const path: any = paths["path/t5.spec.js"];
           expect(path.describe[0]).toBe('\\"s5\\"');
           expect(path.it[0]).toBe('\\"d5\\"');
@@ -84,28 +85,28 @@ describe("Path finder tests", () => {
 
     describe("Test files with multiple test cases", () => {
       it("6th test file match suite 6 and description 6.1", () => {
-        const paths: any = pathFinder.parseTestFiles("**/*.spec.ts", "utf-8");
+        const paths: any = pathFinder.getTestFilesPaths("**/*.spec.ts", "utf-8");
         const path: any = paths["path/t6.spec.js"];
         expect(path.describe[0]).toBe("s6");
         expect(path.it[0]).toBe("d6.1");
       });
 
       it("6th test file match suite 6.2 and description 6.2 (sibling)", () => {
-        const paths: any = pathFinder.parseTestFiles("**/*.spec.ts", "utf-8");
+        const paths: any = pathFinder.getTestFilesPaths("**/*.spec.ts", "utf-8");
         const path: any = paths["path/t6.spec.js"];
         expect(path.describe[1]).toBe("s6.2");
         expect(path.it[1]).toBe("d6.2");
       });
 
       it("6th test file match suite 6.3 and description 6.3 (sibling)", () => {
-        const paths: any = pathFinder.parseTestFiles("**/*.spec.ts", "utf-8");
+        const paths: any = pathFinder.getTestFilesPaths("**/*.spec.ts", "utf-8");
         const path: any = paths["path/t6.spec.js"];
         expect(path.describe[2]).toBe("s6.3");
         expect(path.it[2]).toBe("d6.3");
       });
 
       it("6th test file match suite 6.4.1 and description 6.4.1 (nested)", () => {
-        const paths: any = pathFinder.parseTestFiles("**/*.spec.ts", "utf-8");
+        const paths: any = pathFinder.getTestFilesPaths("**/*.spec.ts", "utf-8");
         const path: any = paths["path/t6.spec.js"];
         expect(path.describe[4]).toBe("s6.4.1");
         expect(path.it[3]).toBe("d6.4.1");
@@ -115,38 +116,38 @@ describe("Path finder tests", () => {
 
   describe("Find test file path tests", () => {
     it("Test file path not found", () => {
-      expect(pathFinder.testFile(parsedTestFiles, "s7", "d7")).toBeUndefined();
+      expect(pathFinder.getTestFilePath(parsedTestFiles, "s7", "d7")).toBeUndefined();
     });
 
     it("Suite 1 and description 1 found in test file 1", () => {
-      expect(pathFinder.testFile(parsedTestFiles, "s1", "d1")).toBe("path/t1.spec.js");
+      expect(pathFinder.getTestFilePath(parsedTestFiles, "s1", "d1")).toBe("path/t1.spec.js");
     });
 
     it("Suite 2 and description 2 found in test file 2", () => {
-      expect(pathFinder.testFile(parsedTestFiles, "s2", "d2")).toBe("path/t2.spec.js");
+      expect(pathFinder.getTestFilePath(parsedTestFiles, "s2", "d2")).toBe("path/t2.spec.js");
     });
 
     it("Suite 3 and description 3 found in test file 3.1", () => {
-      expect(pathFinder.testFile(parsedTestFiles, "s3", "d3.1")).toBe("path/t3.spec.js");
+      expect(pathFinder.getTestFilePath(parsedTestFiles, "s3", "d3.1")).toBe("path/t3.spec.js");
     });
 
     it("Suite 3 and description 3 found in test file 3.2", () => {
-      expect(pathFinder.testFile(parsedTestFiles, "s3", "d3.2")).toBe("path/t3.spec.js");
+      expect(pathFinder.getTestFilePath(parsedTestFiles, "s3", "d3.2")).toBe("path/t3.spec.js");
     });
 
     it("Suite 4 and description 4 found in test file 4", () => {
-      expect(pathFinder.testFile(parsedTestFiles, "\\'s4\\'", "\\'d4\\'")).toBe("path/t4.spec.js");
+      expect(pathFinder.getTestFilePath(parsedTestFiles, "\\'s4\\'", "\\'d4\\'")).toBe("path/t4.spec.js");
     });
 
     it("Suite 5 and description 5 found in test file 5", () => {
-      expect(pathFinder.testFile(parsedTestFiles, '\\"s5\\"', '\\"d5\\"')).toBe("path/t5.spec.js");
+      expect(pathFinder.getTestFilePath(parsedTestFiles, '\\"s5\\"', '\\"d5\\"')).toBe("path/t5.spec.js");
     });
 
     it("Suite 6 and description 6 found in test file 6", () => {
-      expect(pathFinder.testFile(parsedTestFiles, "s6", "d6.1")).toBe("path/t6.spec.js");
-      expect(pathFinder.testFile(parsedTestFiles, "s62", "d6.2")).toBe("path/t6.spec.js");
-      expect(pathFinder.testFile(parsedTestFiles, "s63", "d6.3")).toBe("path/t6.spec.js");
-      expect(pathFinder.testFile(parsedTestFiles, "s6.4.1text", "d6.4.1text")).toBe("path/t6.spec.js");
+      expect(pathFinder.getTestFilePath(parsedTestFiles, "s6", "d6.1")).toBe("path/t6.spec.js");
+      expect(pathFinder.getTestFilePath(parsedTestFiles, "s62", "d6.2")).toBe("path/t6.spec.js");
+      expect(pathFinder.getTestFilePath(parsedTestFiles, "s63", "d6.3")).toBe("path/t6.spec.js");
+      expect(pathFinder.getTestFilePath(parsedTestFiles, "s6.4.1text", "d6.4.1text")).toBe("path/t6.spec.js");
     });
   });
 });
--- src/core/shared/path-finder.ts
+++ src/core/shared/path-finder.ts
@@ -1,18 +1,23 @@
-const fs = require("fs");
+import { FileHelper } from "./../integration/file-helper";
 const glob = require("glob");
 
 export class PathFinder {
-  public parseTestFiles(pattern: string, encoding: string) {
+  private readonly regexPattern: RegExp = /((describe)|(it))\s*\(\s*((?<![\\])[\`\'\"])((?:.(?!(?<![\\])\4))*.?)\4/gi;
+  private readonly describeType = "describe";
+  private readonly itType = "it";
+  public constructor(private readonly fileHelper: FileHelper) {}
+
+  public getTestFilesPaths(pattern: string, encoding: string) {
     const paths = {};
     const results = glob.sync(pattern);
     results.find((path: any, index: any, array: any) => {
-      this.parseTestFile(paths, path, this.testFileData(path, encoding));
+      this.parseTestFile(paths, path, this.getTestFileData(path, encoding));
     });
 
     return paths;
   }
 
-  public testFile(paths: any, describe: any, it: any) {
+  public getTestFilePath(paths: any, describe: any, it: any) {
     const testFile = Object.keys(paths).find(path => this.exist(paths, path, describe, it));
     if (testFile === undefined) {
       global.console.log("Test file path not found!" + JSON.stringify(paths) + "|" + describe + "|" + it);
@@ -20,24 +25,22 @@ export class PathFinder {
     return testFile;
   }
 
-  private testFileData(path: any, encoding: any) {
-    return this.removeNewLines(this.removeComments(fs.readFileSync(path, encoding)));
+  private getTestFileData(path: any, encoding: any) {
+    return this.removeNewLines(this.removeComments(this.fileHelper.readFile(path, encoding)));
   }
 
   private parseTestFile(paths: any, path: any, data: any) {
     let result: any;
-    const regex = this.regexPattern();
-    while (result != null) {
-      result = regex.exec(data);
+    while ((result = this.regexPattern.exec(data)) != null) {
       const type = result[2] || result[3];
       const text = result[5];
       if (paths[path] === undefined) {
         paths[path] = { describe: [], it: [] };
       }
-      if (type === "describe") {
+      if (type === this.describeType) {
         paths[path].describe.push(text);
       }
-      if (type === "it") {
+      if (type === this.itType) {
         paths[path].it.push(text);
       }
     }
@@ -48,20 +51,22 @@ export class PathFinder {
   }
 
   private existDescribe(paths: any, path: any, describe: any) {
-    return paths[path].describe.find((element: any) => describe.startsWith(this.removeEscapedQuotes(element)));
+    return paths[path].describe.some((element: any) => {
+      // const elementWithoutQuotes = this.removeEscapedQuotes(element);
+      return describe.startsWith(element);
+    });
   }
 
   private existIt(paths: any, path: any, it: any) {
-    return paths[path].it.find((element: any) => it.startsWith(this.removeEscapedQuotes(element)));
+    return paths[path].it.some((element: any) => {
+      // const elementWithoutQuotes = this.removeEscapedQuotes(element);
+      return it.startsWith(element);
+    });
   }
 
-  private regexPattern() {
-    return /((describe)|(it))\s*\(\s*((?<![\\])[\`\'\"])((?:.(?!(?<![\\])\4))*.?)\4/gi;
-  }
-
-  private removeEscapedQuotes(str: any) {
-    return str.replace(/(?:\\|\\\\)((")|(')|(`))/g, "$1");
-  }
+  // private removeEscapedQuotes(str: any) {
+  //   return str.replace(/(?:\\|\\\\)((")|(')|(`))/g, "$1");
+  // }
 
   private removeComments(data: any) {
     return data.replace(/\/\*[\s\S]*?\*\/|([^:]|^)\/\/.*$/gm, "");
