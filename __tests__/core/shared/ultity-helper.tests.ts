ef4b9f1f2f6427a80b726a8466fcfab11ef1041d
Patricio Ferraggi <pattferraggi@gmail.com>
2019-06-26 19:55:29 +0200

    cleanup names

--- __tests__/core/shared/ultity-helper.tests.ts
+++ __tests__/core/shared/ultity-helper.tests.ts
@@ -1,8 +1,8 @@
-import { TestExplorerHelper } from "../../../src/core/test-explorer/test-explorer-helper";
+import { UtilityHelper } from "../../../src/core/shared/utility-helper";
 
 test("groupBy should return grouped values by the used key", () => {
   // Arrange
-  const helper = new TestExplorerHelper();
+  const helper = new UtilityHelper();
   const mappedValues = [{ property: "value" }, { property: "value" }];
   const expectedResult = { value: [{ property: "value" }, { property: "value" }] };
 
@@ -15,7 +15,7 @@ test("groupBy should return grouped values by the used key", () => {
 
 test("removeElementsFromArrayWithoutModifyingIt should return an array without the pass values if they re text", () => {
   // Arrange
-  const helper = new TestExplorerHelper();
+  const helper = new UtilityHelper();
   const completeSetOfValues = ["kjhtml", "progress", "dot"];
   const removeValues = ["kjhtml", "dot"];
   const expectedResults = ["progress"];
@@ -29,7 +29,7 @@ test("removeElementsFromArrayWithoutModifyingIt should return an array without t
 
 test("removeElementsFromArrayWithoutModifyingIt should return an array without the pass values if they re objects", () => {
   // Arrange
-  const helper = new TestExplorerHelper();
+  const helper = new UtilityHelper();
   const completeSetOfValues = [{ kjhtml: "43" }, { progress: "2" }, { dot: "3" }];
   const removeValues = [{ kjhtml: "43" }, { dot: "3" }];
   const expectedResults = [{ progress: "2" }];
@@ -43,7 +43,7 @@ test("removeElementsFromArrayWithoutModifyingIt should return an array without t
 
 test("removeElementsFromArrayWithoutModifyingIt should return an empty array if we pass an undefined set of values", () => {
   // Arrange
-  const helper = new TestExplorerHelper();
+  const helper = new UtilityHelper();
 
   // Act
   const result = helper.removeElementsFromArrayWithoutModifyingIt(undefined, undefined);
@@ -54,7 +54,7 @@ test("removeElementsFromArrayWithoutModifyingIt should return an empty array if 
 
 test("removeElementsFromArrayWithoutModifyingIt should return an array without the pass value if its only one value", () => {
   // Arrange
-  const helper = new TestExplorerHelper();
+  const helper = new UtilityHelper();
   const completeSetOfValues = ["43", "2", "3"];
   const removeValues = "43";
   const expectedResults = ["2", "3"];
