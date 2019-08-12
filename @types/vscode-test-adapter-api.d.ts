8f82ffdc616b28757c5b1f762e190ebe92734f3f
Patricio Ferraggi <pattferraggi@gmail.com>
2019-08-12 23:26:46 +0200

    make fullName in testsuiteInfo optional

--- @types/vscode-test-adapter-api.d.ts
+++ @types/vscode-test-adapter-api.d.ts
@@ -2,6 +2,6 @@ import * as VSCodeTestAPI from "vscode-test-adapter-api";
 
 declare module "vscode-test-adapter-api" {
   export interface TestSuiteInfo extends VSCodeTestAPI.TestSuiteInfo {
-    fullName: string;
+    fullName?: string;
   }
 }
